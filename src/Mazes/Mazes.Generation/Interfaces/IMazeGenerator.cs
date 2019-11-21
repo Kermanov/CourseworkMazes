7cf2436859613636b2f33e832d1cbe30237cf815
Maksym Dovhan <kerman901@gmail.com>
2019-11-21 03:18:11 +0200

    Add maze generator interface

---
+++ src/Mazes/Mazes.Generation/Interfaces/IMazeGenerator.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Mazes.Generation.Interfaces
+{
+    public interface IMazeGenerator
+    {
+        Maze Generate(int width, int height);
+    }
+}
