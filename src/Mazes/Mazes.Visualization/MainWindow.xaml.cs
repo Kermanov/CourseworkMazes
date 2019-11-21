a6bc6b46880bf8cda835d14fbb697e9b449cd1d3
Maksym Dovhan <kerman901@gmail.com>
2019-11-22 01:11:58 +0200

    Add UI

--- src/Mazes/Mazes.Visualization/MainWindow.xaml.cs
+++ src/Mazes/Mazes.Visualization/MainWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿using Mazes.Generation.MazeGenerators;
+﻿using Mazes.Generation;
+using Mazes.Generation.Interfaces;
+using Mazes.Generation.MazeGenerators;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -21,9 +23,57 @@ namespace Mazes.Visualization
     /// </summary>
     public partial class MainWindow : Window
     {
+        private MazeCanvas mazeCanvas;
+
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private async void generateButton_Click(object sender, RoutedEventArgs e)
+        {
+            IMazeGenerator mazeGenerator = null;
+            if (mazeTypeComboBox.SelectedIndex == 0)
+            {
+                mazeGenerator = new PerfectMazeGenerator();
+            }
+
+            try
+            {
+                int width = int.Parse(widthTextBox.Text);
+                int height = int.Parse(heightTextBox.Text);
+
+                var maze = await Task.Run(() => mazeGenerator.Generate(width, height));
+
+                if (mazeCanvas == null)
+                {
+                    mazeCanvas = new MazeCanvas(maze);
+                    mainGrid.Children.Add(mazeCanvas);
+                }
+                else
+                {
+                    mazeCanvas.Maze = maze;
+                }
+            }
+            catch
+            {
+
+            }
+        }
+
+        private void mazeStyleComboBox_DropDownClosed(object sender, EventArgs e)
+        {
+            if (mazeCanvas != null)
+            {
+                if (mazeStyleComboBox.SelectedIndex == 0)
+                {
+                    mazeCanvas.MazeStyle = MazeStyle.Default;
+                }
+                else if (mazeStyleComboBox.SelectedIndex == 1)
+                {
+                    mazeCanvas.MazeStyle = MazeStyle.Bold;
+                }
+            }
         }
     }
 }
