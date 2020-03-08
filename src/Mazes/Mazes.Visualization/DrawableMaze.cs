f518e11c1d19389f7c65fbb7f24de5f6f534bc85
Maksym Dovhan <kerman901@gmail.com>
2020-03-08 20:05:23 +0200

    Add simple UI

---
+++ src/Mazes/Mazes.Visualization/DrawableMaze.cs
@@ -0,0 +1,90 @@
+﻿using Mazes.Generation;
+using SFML.Graphics;
+using SFML.System;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mazes.Visualization
+{
+    class DrawableMaze : Drawable
+    {
+        private readonly Maze maze;
+        private readonly float cellWidth;
+        private readonly float cellHeight;
+        private readonly float lineThickness;
+
+        public Vector2f Position { get; set; }
+
+        public DrawableMaze(Maze maze, int pixelWidth, int pixelHeight, float lineThickness)
+        {
+            this.maze = maze;
+            this.lineThickness = lineThickness;
+            cellWidth = pixelWidth / maze.Width;
+            cellHeight = pixelHeight / maze.Height;
+        }
+
+        private void DrawCell(int row, int col, RenderTarget target)
+        {
+            var cell = maze[new CellPosition(row, col)];
+
+            if (cell.TopSide == SideState.Closed)
+            {
+                var rect = new RectangleShape
+                {
+                    Position = new Vector2f(col * cellWidth + Position.X, row * cellHeight + Position.Y),
+                    Size = new Vector2f(cellWidth + lineThickness, lineThickness),
+                    FillColor = Color.Black
+                };
+                target.Draw(rect);
+            }
+            if (cell.LeftSide == SideState.Closed)
+            {
+                var rect = new RectangleShape
+                {
+                    Position = new Vector2f(col * cellWidth + Position.X, row * cellHeight + Position.Y),
+                    Size = new Vector2f(lineThickness, cellHeight + lineThickness),
+                    FillColor = Color.Black
+                };
+                target.Draw(rect);
+            }
+        }
+
+        public void Draw(RenderTarget target, RenderStates states)
+        {
+            var background = new RectangleShape
+            {
+                Position = Position,
+                Size = new Vector2f(cellWidth * maze.Width, cellHeight * maze.Height),
+                FillColor = Color.White
+            };
+            target.Draw(background);
+
+            for (int i = 0; i < maze.Height; ++i)
+            {
+                for (int j = 0; j < maze.Width; ++j)
+                {
+                    DrawCell(i, j, target);
+                }
+            }
+
+            var bottomLine = new RectangleShape
+            {
+                Position = new Vector2f(Position.X, cellHeight * maze.Height + Position.Y),
+                Size = new Vector2f(cellWidth * maze.Width + lineThickness, lineThickness),
+                FillColor = Color.Black
+            };
+            target.Draw(bottomLine);
+
+            var rightLine = new RectangleShape
+            {
+                Position = new Vector2f(cellWidth * maze.Width + Position.X, Position.Y),
+                Size = new Vector2f(lineThickness, cellHeight * maze.Height + lineThickness),
+                FillColor = Color.Black
+            };
+            target.Draw(rightLine);
+        }
+    }
+}
--- src/Mazes/Mazes.Visualization/Program.cs
+++ src/Mazes/Mazes.Visualization/Program.cs
@@ -3,13 +3,62 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Mazes.Generation;
+using Mazes.Generation.MazeGenerators;
+using SFML;
+using SFML.Graphics;
+using SFML.System;
+using SFML.Window;
+using TGUI;
 
 namespace Mazes.Visualization
 {
     class Program
     {
+        private static RenderWindow window;
+        private static DrawableMaze drawableMaze;
         static void Main(string[] args)
         {
+            window = new RenderWindow(VideoMode.FullscreenModes[0], "Mazes", Styles.Fullscreen);
+            window.Closed += (s, e) => window.Close();
+            window.SetActive();
+
+            NewDrawableMaze();
+
+            var gui = new Gui(window);
+
+            var generateButton = new Button
+            {
+                Text = "Generate",
+                Size = new Vector2f(window.Size.X / 10, window.Size.Y / 10),
+                Position = new Vector2f(window.Size.X * 9 / 10, 10)
+            };
+            generateButton.Pressed += (s, e) =>
+            {
+                NewDrawableMaze();
+            };
+
+            gui.Add(generateButton);
+
+
+            while (window.IsOpen)
+            {
+                window.Clear(new Color(232, 232, 232));
+                window.DispatchEvents();
+
+                window.Draw(drawableMaze);
+                gui.Draw();
+
+                window.Display();
+            }
+        }
+
+        private static void NewDrawableMaze()
+        {
+            var mazeGenerator = new PrimaModifiedGenerator();
+            var maze = mazeGenerator.Generate(40, 40);
+            drawableMaze = new DrawableMaze(maze, (int)window.Size.Y - 20, (int)window.Size.Y - 20, 5);
+            drawableMaze.Position = new Vector2f(10, 10);
         }
     }
 }
