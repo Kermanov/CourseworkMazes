73341b550478f7cfd0baedea05b702e5dbe10325
Maksym Dovhan <kerman901@gmail.com>
2020-05-17 01:08:19 +0300

    Add final solution calculating for WallFollowerSolver

--- src/Mazes/Mazes.Solving/MazeSolvers/WallFollowerSolver.cs
+++ src/Mazes/Mazes.Solving/MazeSolvers/WallFollowerSolver.cs
@@ -24,6 +24,7 @@ namespace Mazes.Solving.MazeSolvers
         private List<CellPosition> visited;
 
         public List<CellPosition> Solution { get; private set; }
+        public List<CellPosition> FinalSolution { get; private set; }
 
         public WallFollowerSolver(TurningDirection turningDirection)
         {
@@ -43,8 +44,10 @@ namespace Mazes.Solving.MazeSolvers
             {
                 Move();
             }
+            visited.Add(new CellPosition(maze.Height - 1, maze.Width - 1));
 
             Solution = visited;
+            FinalSolution = FinalPath();
         }
 
         private void Move()
@@ -147,6 +150,49 @@ namespace Mazes.Solving.MazeSolvers
             deltaRow = -deltaRow;
 
             directionNumber = (directionNumber + 2) % 4;
+        }
+
+        private List<CellPosition> FinalPath()
+        {
+            var finalPath = new List<CellPosition>();
+            for (int i = 0; i < visited.Count; ++i)
+            {
+                var cell = visited[i];
+                finalPath.Add(cell);
+                if (visited.FindAll(c => c == cell).Count > 1)
+                {
+                    var nextCells = GetNextOpenCellPositions(cell);
+                    nextCells.Remove(finalPath.Last());
+                    var validCellIndex = nextCells.Max(c => visited.IndexOf(c));
+                    i = validCellIndex - 1;
+                }
+            }
+
+            return finalPath;
+        }
+
+        protected List<CellPosition> GetNextOpenCellPositions(CellPosition cellPosition)
+        {
+            var nextCellPositions = new List<CellPosition>();
+
+            if (cellPosition.Row > 0 && maze[cellPosition].TopSide == SideState.Open)
+            {
+                nextCellPositions.Add(new CellPosition(cellPosition.Row - 1, cellPosition.Col));
+            }
+            if (cellPosition.Row < maze.Height - 1 && maze[cellPosition].BottomSide == SideState.Open)
+            {
+                nextCellPositions.Add(new CellPosition(cellPosition.Row + 1, cellPosition.Col));
+            }
+            if (cellPosition.Col > 0 && maze[cellPosition].LeftSide == SideState.Open)
+            {
+                nextCellPositions.Add(new CellPosition(cellPosition.Row, cellPosition.Col - 1));
+            }
+            if (cellPosition.Col < maze.Width - 1 && maze[cellPosition].RightSide == SideState.Open)
+            {
+                nextCellPositions.Add(new CellPosition(cellPosition.Row, cellPosition.Col + 1));
+            }
+
+            return nextCellPositions;
         }
     }
 }
